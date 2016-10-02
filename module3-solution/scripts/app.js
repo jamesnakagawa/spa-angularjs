3b366b7b96a3360e8db807b96a21c28a88ec618a
James Nakagawa <james@nakagawa.ca>
2016-10-01 21:08:13 -0700

    Module 3 solution

---
+++ module3-solution/scripts/app.js
@@ -0,0 +1,63 @@
+(function() {
+"use strict";
+
+angular.module('NarrowItDownApp',[])
+  .controller('NarrowItDownController', NarrowItDownController)
+  .service('MenuSearchService', MenuSearchService)
+  .constant('MenuApiUrl', 'https://davids-restaurant.herokuapp.com/')
+  .directive('foundItems', FoundItemsDirective);
+
+function FoundItemsDirective() {
+  var ddo = {
+    templateUrl: '../templates/founditems.template.html',
+    restrict: 'E',
+    scope: {
+      items: "<foundItems",
+      onRemove: "&"
+    },
+    controller: FoundItemsDirCtrl,
+    controllerAs: 'fidc',
+    bindToController: true
+  };
+  return ddo;
+}
+
+function FoundItemsDirCtrl() {
+  var fidc = this;
+}
+
+MenuSearchService.$inject = ['$http', 'MenuApiUrl'];
+function MenuSearchService($http, MenuApiUrl) {
+  this.getMatchedMenuItems = function(searchTerm) {
+    return $http({
+      method: 'GET',
+      url: MenuApiUrl + 'menu_items.json'
+    }).then(function(result) {
+      var items = result.data.menu_items;
+      var found = items.filter(function(el) {
+        return el.description.indexOf(searchTerm) !== -1 ||
+          el.name.indexOf(searchTerm) !== -1;
+      });
+      return found;
+    });
+  }
+}
+
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService) {
+  var nid = this;
+
+  nid.foundItems = [];
+
+  nid.search = function(term) {
+    MenuSearchService.getMatchedMenuItems(term).then(result => {
+      nid.foundItems = result;
+    });
+  }
+
+  nid.removeItem = function(index) {
+    nid.foundItems.splice(index, 1);
+  }
+}
+
+})();
